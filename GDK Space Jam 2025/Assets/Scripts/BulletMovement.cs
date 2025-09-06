221d634716131882d5ac40f46e705c31a4f2c4cb
AlexPiroth <alexpiroth@gmail.com>
2025-09-06 02:39:48 -0400

    added bullet collision for bouncing projectiles

--- GDK Space Jam 2025/Assets/Scripts/BulletMovement.cs
+++ GDK Space Jam 2025/Assets/Scripts/BulletMovement.cs
@@ -6,24 +6,30 @@ public class BulletMovement : MonoBehaviour
     // Bullet Stats
     public float bulletSpeed;
     public int bulletType;
+    public int bounces;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rigidbody;
+
+    void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddRelativeForce(new Vector2(0,1) * bulletSpeed);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(0, 1, 0));
+        //transform.Translate(new Vector3(0, 0.5f, 0));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.gameObject.layer == 6)
         {
-            Destroy(this.gameObject);
+            if (bounces == 0)
+                Destroy(this.gameObject);
+            else
+                bounces--;
         }
     }
 }
