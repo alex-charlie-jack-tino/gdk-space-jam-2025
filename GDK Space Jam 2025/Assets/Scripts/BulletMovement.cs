84fa32b4760271d8267bca69a01d06312b139bd0
Bug Jones <charlesdheck@gmail.com>
2025-09-06 10:47:43 -0400

    floaty physics and screen wrapping

--- GDK Space Jam 2025/Assets/Scripts/BulletMovement.cs
+++ GDK Space Jam 2025/Assets/Scripts/BulletMovement.cs
@@ -17,5 +17,15 @@ public class BulletMovement : MonoBehaviour
     void FixedUpdate()
     {
         transform.Translate(new Vector3(0, 1, 0));
+        if (transform.position.y > 14 || transform.position.y < -14)
+        {
+            Vector3 newLocation = new Vector3(transform.position.x, transform.position.y * -1, transform.position.z);
+            transform.SetPositionAndRotation(newLocation, transform.rotation);
+        }
+        if (transform.position.x > 29 || transform.position.x < -29)
+        {
+            Vector3 newLocation = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+            transform.SetPositionAndRotation(newLocation, transform.rotation);
+        }
     }
 }
