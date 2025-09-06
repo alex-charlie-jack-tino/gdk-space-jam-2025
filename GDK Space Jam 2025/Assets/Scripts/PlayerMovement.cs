19a42c13fdd7c45f5a563a47dfbcd2ae372fa969
AlexPiroth <alexpiroth@gmail.com>
2025-09-06 19:19:13 -0400

    added physics-based movement

--- GDK Space Jam 2025/Assets/Scripts/PlayerMovement.cs
+++ GDK Space Jam 2025/Assets/Scripts/PlayerMovement.cs
@@ -32,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     float currentSpeed;
     Vector3 rotation;
     bool onCooldown = false;
+    Rigidbody rigidbody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -43,25 +44,27 @@ public class PlayerMovement : MonoBehaviour
     {
         move = InputSystem.actions.FindAction("Move");
         shoot = InputSystem.actions.FindAction("Attack");
+        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
     {
         move.performed += Move;
         move.canceled += Move;
-        //shoot.performed += Shoot;
+        shoot.performed += Shoot;
 
-        //if (weaponType == 4)
-        //{
-        //    shoot.canceled += Shoot;
-        //}
+        if (weaponType == 4)
+        {
+            shoot.canceled += Shoot;
+        }
     }
 
     private void OnDisable()
     {
         move.performed -= Move;
         move.canceled -= Move;
-        //shoot.performed -= Shoot;
+        shoot.performed -= Shoot;
     }
 
     // Update is called once per frame
@@ -78,34 +81,23 @@ public class PlayerMovement : MonoBehaviour
             Vector3 newLocation = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
             transform.SetPositionAndRotation(newLocation, transform.rotation);
         }
-        rotation += deltaRotation;
-        transform.Rotate(rotation);
-        movement += deltaVelocity;
-        float clampedMovement = Mathf.Clamp(movement.magnitude, 0, maxSpeed);
-        movement = movement.normalized * clampedMovement;
-        transform.Translate(movement, Space.World);
+        rigidbody.AddRelativeForce(movement);
+        rigidbody.AddRelativeTorque(rotation);
     }
 
     private void Move(InputAction.CallbackContext ctx)
     {
+        
         //read input
         moveInput = ctx.ReadValue<Vector2>();
         //Forward or Backward
         var moveSpeed = moveInput.y > 0 ? forwardSpeed : backSpeed;
-        //calculate new velocity vector to existing velocity
-        deltaVelocity = transform.up*moveInput.y*moveSpeed;
-        /*
-        if (movement.x > 0)
-        {
-            currentSpeed = forwardSpeed;
-        }
-        else
-        {
-            currentSpeed = backSpeed;
-        }
-        */
+
+        // Calulate force to add to ship
+        movement = new Vector3(0, moveInput.y * moveSpeed, 0);
+
         //add new rotation to existing rotation
-        deltaRotation = new Vector3(moveInput.x * turnSpeed, 0, 0);
+        rotation = new Vector3(moveInput.x * turnSpeed, 0, 0);
     }
 
     private void Shoot(InputAction.CallbackContext ctx)
